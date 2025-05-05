487625510914e4ccea0744220571b9e937c58c75
danielambda <daniel.gevorgyan25@gmail.com>
2025-05-06 01:05:54 +0300

    chore: removed DSL from FSAfe exports

--- src/Telegram/Bot/FSAfe.hs
+++ src/Telegram/Bot/FSAfe.hs
@@ -5,8 +5,6 @@ module Telegram.Bot.FSAfe
   , module Telegram.Bot.FSAfe.RunTG
   , module Telegram.Bot.FSAfe.BotContextParser
   , module Telegram.Bot.FSAfe.Start
-  , module Telegram.Bot.DSL
-  , module Telegram.Bot.DSL.TaggedContext
   ) where
 
 import Telegram.Bot.FSAfe.BotM
@@ -15,5 +13,3 @@ import Telegram.Bot.FSAfe.Reply
 import Telegram.Bot.FSAfe.RunTG
 import Telegram.Bot.FSAfe.BotContextParser
 import Telegram.Bot.FSAfe.Start
-import Telegram.Bot.DSL
-import Telegram.Bot.DSL.TaggedContext
